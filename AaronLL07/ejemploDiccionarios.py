9f613b7f1e33ba0e88d73d07fdc015f4fbd3a317
Aaron Emiliano Lopez Lombera <aaron.lopezl@uanl.edu.mx>
2023-08-23 11:29:09 -0600

    Cambié ejemplos de diccionarios

--- AaronLL07/ejemploDiccionarios.py
+++ AaronLL07/ejemploDiccionarios.py
@@ -1,14 +1,3 @@
-""" canciones = {
-    10: "Otra noche en Miami",
-    20: "La Santa",
-    30: "Agosto",
-    40: "Nataoki",
-    50: "Cuento"
-}
-print(len(canciones))
-print(canciones.keys())
-print(canciones.values()) """
-
 # Ejemplo de diccionario
 
 contactos = {}
@@ -25,3 +14,48 @@ while True:
     contactos[nueva_clave]=(nombre, telefono)
 
 print(contactos)
+
+""" print(contactos[2])  """#Esta manera está bien
+
+#Forma recomendable y la mejor manera de poder ver la posicion del diccinario que desea
+# | = pipe = tubo en python es un operador que su funcion es juntar los diccionarios en un solo conjunto
+# en un conjunto solo se pueden tener elementos inmutables, los conjuntos no garantizan el orden 
+# de los elementos
+
+#
+
+print(contactos.get(2, "NO EXISTE"))
+
+# una operación de conjuntos siempre tiene como resultado un nuevo conjunto
+
+conjunto_uno = {"Hugo", "Paco", "Luis"}
+conjunto_dos = {"Britni", "Llojan", "Luis"}
+
+# Diferencia
+print(conjunto_uno - conjunto_dos)
+
+# Diferenia simétrica
+print(conjunto_uno ^ conjunto_dos)
+
+import random
+valores = [random.randrange(1,6) for valor in range(100)]
+print(valores)
+
+diferentes = set(valores)
+print()
+print(diferentes)
+
+# First In First Out - Primero en llegar es el primero en salir
+
+cola = list()
+
+for cantidad in range(5):
+    nuevo = input("Nombre del recién llegado: ")
+    cola.append(nuevo)
+print(f"Se agregaron {(len(cola))} elementos")
+print("Procedemos a retirarlos de la cola: ")
+while cola:
+    cola.pop(0)
+pass 
+
+# Last In First Out - Apilar objetos - Último en llegar es el primero en salir 
